8fcafcc14b4d763bbd9b6e7146ae205af72094bd
Greg Stimson <stimsong@yahoo.com>
2018-03-25 17:40:41 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,13 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand
-#    BRANDS.uniq
-  end
-
-  def brand=(brand)
-    BRANDS << brand
-#    BRANDS.uniq
+    BRANDS << brand unless BRANDS.include?(brand)
   end
 
   def cobble
